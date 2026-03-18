7aaf274e69df2970ee07d257063b19901992e155
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-03-18 22:54:20 +0300

    Формирование проекта

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def main():
+    try:
+        url = "https://[имя_аккаунта].quickresto.ru/api/v1/clients"
+        headers = {
+            "Authorization": "Bearer your_api_key_here",
+            "Accept": "application/json"
+        }
+        params = {
+            "limit": 50,
+            "offset": 0
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        print(response.json())
+    except requests.exceptions.InvalidURL:
+        print("Неверный URL")
+
+
+if __name__ == '__main__':
+    main()
