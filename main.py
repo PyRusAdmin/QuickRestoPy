9c4381d123f7df3a487b3f6c394207452ed29d6f
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-03-25 22:14:55 +0300

    Структурирование проекта

--- main.py
+++ main.py
@@ -91,31 +91,7 @@ def get_all_clients():
     return all_clients
 
 
-def update_customer_bonus(customer_id: int, amount: float, customer_phone):
-    """Редактирование бонусных балов для клиента"""
-    try:
-        logger.info(f"Редактирование бонусных балов для клиента {customer_id}")
 
-        url = f"https://{layer_name_quickresto}.quickresto.ru/platform/online/bonuses/creditHold"
-
-        body = {
-            "customerToken": {
-                "type": "phone",  # ← тип токена: телефон
-                "entry": "manual",  # ← способ ввода: вручную
-                "key": customer_phone  # ← сам номер телефона
-            },
-            "accountType": {
-                "accountGuid": "bonus_account_type-1"  # ← из данных клиента
-            },
-            "amount": amount
-        }
-
-        response = requests.post(url, json=body, auth=auth, headers=headers, timeout=30)
-        response.raise_for_status()
-        return response.json()
-
-    except Exception as e:
-        logger.exception(e)
 
 
 if __name__ == "__main__":
